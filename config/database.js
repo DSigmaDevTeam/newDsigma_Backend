f7bf4ff9f92aa21d78e446e3eb1f66b4e5e636c7
DSigmaDevelopers <107794228+DSigmaDevelopers@users.noreply.github.com>
2022-07-06 12:50:09 +0530

    many changes made

--- config/database.js
+++ config/database.js
@@ -2,34 +2,34 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 // DB Connection  
-module.exports = new Sequelize('newDsigma', 
-    'postgres',
-    'password', {
-    host: 'localhost',
-    dialect: 'postgres',
-    // user: process.env.DATABASE_USERNAME,
-    operatorAliases: false,
-    logging: false,
+// module.exports = new Sequelize('newDsigma', 
+//     'postgres',
+//     'password', {
+//     host: 'localhost',
+//     dialect: 'postgres',
+//     // user: process.env.DATABASE_USERNAME,
+//     operatorAliases: false,
+//     logging: false,
 
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    },
-});
+//     pool: {
+//         max: 5,
+//         min: 0,
+//         acquire: 30000,
+//         idle: 10000
+//     },
+// });
 
 
 
-// module.exports = new Sequelize(
-//     process.env.DATABASE_URL,
-//     {
-//         logging: false,
-//         dialectOptions:{
-//             ssl:{
-//                 required:true,
-//                 rejectUnauthorized: false
-//             }
-//         }
-//     }
-// );
+module.exports = new Sequelize(
+    process.env.DATABASE_URL,
+    {
+        logging: false,
+        dialectOptions:{
+            ssl:{
+                required:true,
+                rejectUnauthorized: false
+            }
+        }
+    }
+);
